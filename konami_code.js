24e6cfe43a0035b3fde3b2324c881b392e695f53
Amanda Baydar <amandabaydar@gmail.com>
2020-09-24 18:45:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -18,9 +18,3 @@ document.body.addEventListener("keydown", (e) => {
 const key = e.key
 if(key === codes[index]){
   index++; 
-if(index === codes.length){
-  alert("Hurray!");
-index = 0;
-  
-else {index = 0;}
-
